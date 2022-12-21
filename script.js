e51d4591f4e8d9cee86735a2bf44e10db8dfd6d8
James Anh <james.anh184@gmail.com>
2022-12-20 21:19:57 -0800

    file setup

---
+++ script.js
@@ -0,0 +1,8 @@
+// setting up canvas
+window.addEventListener('load', function() {
+    const canvas = document.getElementById('canvas1');
+    // drawing context, a built in object that contains all methods and properties that allow to draw and animate colours, shapes and other graphics on HTML canvas.
+    const ctx = canvas.getContext('2d');
+    canvas.width = 500;
+    
+});
