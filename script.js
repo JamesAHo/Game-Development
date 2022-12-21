c679e8a850439890cf33b668f2651557bce0ae99
James Anh <james.anh184@gmail.com>
2022-12-20 21:38:19 -0800

    created Player and GamePlay OOP object

--- script.js
+++ script.js
@@ -4,5 +4,59 @@ window.addEventListener('load', function() {
     // drawing context, a built in object that contains all methods and properties that allow to draw and animate colours, shapes and other graphics on HTML canvas.
     const ctx = canvas.getContext('2d');
     canvas.width = 500;
-    
+    canvas.height = 500;
+    // Implementation of Object Oriented Programming.
+    // player class 
+    class Player {
+       constructor(game) {
+            this.game = game;
+            this.width = 120;
+            this.height = 190;
+            this.x = 20;
+            this.y = 100;
+            this.speedY = 0;
+       }
+       // methods
+       update() {
+            this.y += this.speedY
+       }
+       draw(context){
+            context.fillRect(this.x, this.y, this.width, this.height);
+       }
+    }
+    class Enemy {
+
+    }
+    class InputHandler {
+
+    }
+    class Projectile {
+
+    }
+    class Particle {
+
+    }
+    class Layer {
+
+    }
+    class Background {
+
+    }
+    class UI {
+
+    }
+    class GamepPlay {
+        constructor(width,height) {
+            this.width = width;
+            this.height = height;
+            this.player = new Player(this);
+        }
+        update() {
+            this.player.update();
+        }
+        draw(context){
+            this.player.draw(context);
+        }
+    }
+    const game = new GamepPlay(canvas.width, canvas.height);
 });
