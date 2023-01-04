5ec97b9b38374a27ab44e4f26c87c1463a458088
James Anh <james.anh184@gmail.com>
2023-01-03 23:23:08 -0800

    implemented key for debug @line 98

--- script.js
+++ script.js
@@ -94,14 +94,14 @@ window.addEventListener('load', function() {
                     this.game.keys.push(e.key)
                 } else if( e.key === ' ') {
                     this.game.player.shootTop();
+                } else if(e.key === 'd'){
+                    this.game.debug = !this.game.debug;
                 }
-                console.log(this.game.keys)
             });
             window.addEventListener('keyup', e => {
                 if(this.game.keys.indexOf(e.key) > -1) {
                     this.game.keys.splice(this.game.keys.indexOf(e.key),1);
                 }
-                console.log(this.game.keys)
             })
             
         }
@@ -242,6 +242,7 @@ window.addEventListener('load', function() {
             this.gameTime = 0;
             this.timeLimit = 5000;
             this.speed = 1;
+            this.debug = true;
 
         }
         update(deltaTime) {
