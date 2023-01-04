2650e7a99f3fb0599cd51af00d2761c82dae246d
James Anh <james.anh184@gmail.com>
2023-01-03 23:19:46 -0800

    added game debug @line 44

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ window.addEventListener('load', function() {
             this.x = 20;
             this.y = 100;
             this.frameX = 0;
-            this.frameY = 1;
+            this.frameY = 0;
             this.maxFrame = 37;
             this.speedY = 0;
             this.maxSpeed = 2;
@@ -41,8 +41,7 @@ window.addEventListener('load', function() {
             }
        }
        draw(context){
-            context.fillStyle = 'black';
-            context.fillRect(this.x, this.y, this.width, this.height);
+            if(this.game.debug) context.strokeRect(this.x, this.y, this.width, this.height);
             context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
             this.projectiles.forEach(projectile => {
                 projectile.draw(context);
